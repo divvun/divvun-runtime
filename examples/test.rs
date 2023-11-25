be9496e3a0ca7092c68f4dabefe1a4ad9976cf02
Brendan Molloy <brendan@necessary.nu>
2023-11-25 18:10:46 +0100

    Be not dumb

--- examples/test.rs
+++ examples/test.rs
@@ -19,24 +19,25 @@ async fn run() -> anyhow::Result<()> {
 }
 
 async fn pipeline(input: String) -> anyhow::Result<String> {
-    let x = Box::pin(divvun::suggest(
-        PathBuf::from("./generator-gramcheck-gt-norm.hfstol"),
-        PathBuf::from("./errors.xml"),
-        Box::pin(async move {
-            Ok(input)
-        }),
-    ));
-    let x = Box::pin(cg3::vislcg3(PathBuf::from("./grammarchecker-release.bin"), x));
-    let x = Box::pin(cg3::vislcg3(PathBuf::from("./grc-disambiguator.bin"), x));
+    let x = Box::pin(hfst::tokenize(PathBuf::from("./tokeniser-gramcheck-gt-desc.pmhfst"), 
+    Box::pin(async move {
+        Ok(input)
+    })));
+    let x = Box::pin(cg3::vislcg3(PathBuf::from("./valency.bin"), x));
+    let x = Box::pin(cg3::vislcg3(PathBuf::from("./mwe-dis.bin"), x));
+    let x = Box::pin(cg3::mwesplit(x));
+    let x = Box::pin(divvun::blanktag(PathBuf::from("./analyser-gt-whitespace.hfst"), x));
     let x = Box::pin(divvun::cgspell(
         PathBuf::from("./errmodel.default.hfst"),
         PathBuf::from("./acceptor.default.hfst"),
         x,
     ));
-    let x = Box::pin(divvun::blanktag(PathBuf::from("./analyser-gt-whitespace.hfst"), x));
-    let x = Box::pin(cg3::mwesplit(x));
-    let x = Box::pin(cg3::vislcg3(PathBuf::from("./mwe-dis.bin"), x));
-    let x = Box::pin(cg3::vislcg3(PathBuf::from("./valency.bin"), x));
-    let x = Box::pin(hfst::tokenize(PathBuf::from("./tokeniser-gramcheck-gt-desc.pmhfst"), x));
+    let x = Box::pin(cg3::vislcg3(PathBuf::from("./grc-disambiguator.bin"), x));
+    let x = Box::pin(cg3::vislcg3(PathBuf::from("./grammarchecker-release.bin"), x));
+    let x = Box::pin(divvun::suggest(
+        PathBuf::from("./generator-gramcheck-gt-norm.hfstol"),
+        PathBuf::from("./errors.xml"),
+        x,
+    ));
     Ok(x.await?)
 }
