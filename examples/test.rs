c0ae721a565a9c41116cfc996b845c593353b7f5
Brendan Molloy <brendan@necessary.nu>
2023-11-25 18:25:09 +0100

    wat

--- examples/test.rs
+++ examples/test.rs
@@ -1,6 +1,6 @@
 use std::path::{Path, PathBuf};
 
-use divvun_runtime::modules::*;
+use divvun_runtime::{modules::*, ast::{from_ast, PipelineDefinition}};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -19,25 +19,39 @@ async fn run() -> anyhow::Result<()> {
 }
 
 async fn pipeline(input: String) -> anyhow::Result<String> {
-    let x = Box::pin(hfst::tokenize(PathBuf::from("./tokeniser-gramcheck-gt-desc.pmhfst"), 
-    Box::pin(async move {
+    let jd = &mut serde_json::Deserializer::from_str(include_str!("./ast.json"));
+    let defn: PipelineDefinition = serde_path_to_error::deserialize(jd)?;
+    
+    // let defn: PipelineDefinition = serde_json::from_str().unwrap();
+    let result = from_ast(defn.ast, Box::pin(async {
         Ok(input)
-    })));
-    let x = Box::pin(cg3::vislcg3(PathBuf::from("./valency.bin"), x));
-    let x = Box::pin(cg3::vislcg3(PathBuf::from("./mwe-dis.bin"), x));
-    let x = Box::pin(cg3::mwesplit(x));
-    let x = Box::pin(divvun::blanktag(PathBuf::from("./analyser-gt-whitespace.hfst"), x));
-    let x = Box::pin(divvun::cgspell(
-        PathBuf::from("./errmodel.default.hfst"),
-        PathBuf::from("./acceptor.default.hfst"),
-        x,
-    ));
-    let x = Box::pin(cg3::vislcg3(PathBuf::from("./grc-disambiguator.bin"), x));
-    let x = Box::pin(cg3::vislcg3(PathBuf::from("./grammarchecker-release.bin"), x));
-    let x = Box::pin(divvun::suggest(
-        PathBuf::from("./generator-gramcheck-gt-norm.hfstol"),
-        PathBuf::from("./errors.xml"),
-        x,
-    ));
-    Ok(x.await?)
+    }))?.await?;
+    Ok(result)
+    // let x = Box::pin(hfst::tokenize(
+    //     PathBuf::from("./tokeniser-gramcheck-gt-desc.pmhfst"),
+    //     Box::pin(async move { Ok(input) }),
+    // ));
+    // let x = Box::pin(cg3::vislcg3(PathBuf::from("./valency.bin"), x));
+    // let x = Box::pin(cg3::vislcg3(PathBuf::from("./mwe-dis.bin"), x));
+    // let x = Box::pin(cg3::mwesplit(x));
+    // let x = Box::pin(divvun::blanktag(
+    //     PathBuf::from("./analyser-gt-whitespace.hfst"),
+    //     x,
+    // ));
+    // let x = Box::pin(divvun::cgspell(
+    //     PathBuf::from("./errmodel.default.hfst"),
+    //     PathBuf::from("./acceptor.default.hfst"),
+    //     x,
+    // ));
+    // let x = Box::pin(cg3::vislcg3(PathBuf::from("./grc-disambiguator.bin"), x));
+    // let x = Box::pin(cg3::vislcg3(
+    //     PathBuf::from("./grammarchecker-release.bin"),
+    //     x,
+    // ));
+    // let x = Box::pin(divvun::suggest(
+    //     PathBuf::from("./generator-gramcheck-gt-norm.hfstol"),
+    //     PathBuf::from("./errors.xml"),
+    //     x,
+    // ));
+    // Ok(x.await?)
 }
--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,9 +1,4 @@
-use std::{
-    collections::HashMap,
-    future::{Future, IntoFuture},
-    path::{Path, PathBuf},
-    pin::Pin,
-};
+use std::{collections::HashMap, path::PathBuf};
 
 use serde::{Deserialize, Serialize};
 
@@ -15,7 +10,7 @@ pub struct PipelineDefinition {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type")]
+#[serde(untagged)]
 pub enum Command {
     #[serde(rename = "command")]
     Command {
@@ -36,33 +31,52 @@ pub struct Arg {
 
 // FromAst magic
 
-fn from_ast(command: Command) -> anyhow::Result<InputFut<String>> {
+pub fn from_ast(command: Command, entry_input: InputFut<String>) -> anyhow::Result<InputFut<String>> {
     match command {
         Command::Command {
             cmd,
             mut args,
             input,
         } => match &*cmd {
+            "cg3::mwesplit" => {
+                return Ok(Box::pin(crate::modules::cg3::mwesplit(from_ast(*input.unwrap(), entry_input)?)))
+            }
             "cg3::vislcg3" => {
                 let model_path = PathBuf::from(&args.remove("model_path").unwrap().value.unwrap());
                 return Ok(Box::pin(
-                    crate::modules::cg3::vislcg3(model_path, from_ast(*input.unwrap())?)
-                        .into_future(),
+                    crate::modules::cg3::vislcg3(model_path, from_ast(*input.unwrap(), entry_input)?)
                 ));
             }
             "divvun::blanktag" => {
                 let model_path = PathBuf::from(&args.remove("model_path").unwrap().value.unwrap());
                 return Ok(Box::pin(
-                    crate::modules::divvun::blanktag(model_path, from_ast(*input.unwrap())?)
-                        .into_future(),
+                    crate::modules::divvun::blanktag(model_path, from_ast(*input.unwrap(), entry_input)?)
                 ));
+            }
+            "divvun::cgspell" => {
+                let err_model_path = PathBuf::from(&args.remove("err_model_path").unwrap().value.unwrap());
+                let acc_model_path = PathBuf::from(&args.remove("acc_model_path").unwrap().value.unwrap());
+                return Ok(Box::pin(
+                    crate::modules::divvun::cgspell(err_model_path, acc_model_path, from_ast(*input.unwrap(), entry_input)?)
+                ))
+            }
+            "divvun::suggest" => {
+                let model_path = PathBuf::from(&args.remove("model_path").unwrap().value.unwrap());
+                let error_xml_path = PathBuf::from(&args.remove("error_xml_path").unwrap().value.unwrap());
+                return Ok(Box::pin(
+                    crate::modules::divvun::suggest(model_path, error_xml_path, from_ast(*input.unwrap(), entry_input)?)
+                ))
+            }
+            "hfst::tokenize" => {
+                let model_path = PathBuf::from(&args.remove("model_path").unwrap().value.unwrap());
+                return Ok(Box::pin(
+                    crate::modules::hfst::tokenize(model_path, from_ast(*input.unwrap(), entry_input)?)
+                ))
             }
             _ => {
                 panic!("Unknown command: {}", cmd);
             }
         },
-        Command::Entry { type_value: _lol } => Ok(Box::pin(async {
-            Ok("This is an example string".to_string())
-        })),
+        Command::Entry { type_value: _lol } => Ok(entry_input),
     }
 }
