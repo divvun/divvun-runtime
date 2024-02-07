367510c5a4ac3caeba5892a70376e80313836d42
Brendan Molloy <brendan@necessary.nu>
2024-02-07 02:11:24 +0100

    Add spell

---
+++ src/modules/spell.rs
@@ -0,0 +1,104 @@
+use std::{collections::HashMap, sync::Arc, thread::JoinHandle};
+
+use async_trait::async_trait;
+use divvunspell::{
+    speller::Speller,
+    transducer::{thfst::MemmapThfstTransducer, Transducer},
+    vfs::Fs,
+};
+use tokio::sync::{
+    mpsc::{self, Receiver, Sender},
+    Mutex,
+};
+
+use crate::{
+    ast,
+    modules::{Arg, Command, Module, Ty},
+};
+
+use super::{CommandRunner, Context, Input, InputFut};
+
+inventory::submit! {
+    Module {
+        name: "spell",
+        commands: &[
+            Command {
+                name: "suggest",
+                args: &[
+                    Arg { name: "lexicon_path", ty: Ty::Path },
+                    Arg { name: "mutator_path", ty: Ty::Path },
+                ],
+                init: Suggest::new,
+            },
+        ]
+    }
+}
+
+struct Suggest {
+    _context: Arc<Context>,
+    input_tx: Sender<Option<String>>,
+    output_rx: Mutex<Receiver<Option<String>>>,
+    _thread: JoinHandle<()>,
+}
+
+impl Suggest {
+    pub fn new(
+        context: Arc<Context>,
+        mut kwargs: HashMap<String, ast::Arg>,
+    ) -> Result<Arc<dyn CommandRunner>, anyhow::Error> {
+        let lexicon_path = kwargs
+            .remove("lexicon_path")
+            .and_then(|x| x.value)
+            .ok_or_else(|| anyhow::anyhow!("lexicon_path missing"))?;
+        let mutator_path = kwargs
+            .remove("mutator_path")
+            .and_then(|x| x.value)
+            .ok_or_else(|| anyhow::anyhow!("mutator_path missing"))?;
+
+        let lexicon_path = context.extract_to_temp_dir(lexicon_path)?;
+        let mutator_path = context.extract_to_temp_dir(mutator_path)?;
+
+        let (input_tx, mut input_rx) = mpsc::channel(1);
+        let (output_tx, output_rx) = mpsc::channel(1);
+
+        let thread = std::thread::spawn(move || {
+            let lexicon = MemmapThfstTransducer::from_path(&Fs, lexicon_path).unwrap();
+            let mutator = MemmapThfstTransducer::from_path(&Fs, mutator_path).unwrap();
+            let speller = divvunspell::speller::HfstSpeller::new(mutator, lexicon);
+
+            loop {
+                let Some(Some(input)): Option<Option<String>> = input_rx.blocking_recv() else {
+                    break;
+                };
+
+                let results = speller.clone().suggest(&input);
+                let results = serde_json::to_string(&results).unwrap();
+
+                output_tx.blocking_send(Some(results)).unwrap();
+            }
+        });
+
+        Ok(Arc::new(Self {
+            _context: context,
+            input_tx,
+            output_rx: Mutex::new(output_rx),
+            _thread: thread,
+        }) as _)
+    }
+}
+
+#[async_trait(?Send)]
+impl CommandRunner for Suggest {
+    async fn forward(self: Arc<Self>, input: InputFut) -> Result<Input, anyhow::Error> {
+        let input = input.await?.try_into_string()?;
+
+        self.input_tx
+            .send(Some(input))
+            .await
+            .expect("input tx send");
+        let mut output_rx = self.output_rx.lock().await;
+        let value = output_rx.recv().await.expect("output rx recv");
+
+        Ok(value.unwrap_or_default().into())
+    }
+}
