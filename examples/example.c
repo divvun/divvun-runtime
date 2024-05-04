99f20f435444195d590546c6ed6c3cd5f32578d5
Brendan Molloy <brendan@bbqsrc.net>
2024-05-04 18:48:59 +0200

    Make it work on Windows more

--- examples/example.c
+++ examples/example.c
@@ -8,9 +8,10 @@ static void err_handler(char *_Nullable msg, rust_usize_t len)
 
 int main()
 {
-    bundle_t *bundle = dr__bundle__from_path(dr__slice__new_str("../../../pipeline-examples/tts"), &err_handler);
+    bundle_t *bundle = dr__bundle__from_bundle(dr__slice__new_str("C:\\Users\\Brendan\\Downloads\\tts (2).drb"), &err_handler);
     rust_slice_t slice = dr__bundle__run_pipeline_bytes(bundle, dr__slice__new_str("hello world"), &err_handler);
     printf("We're here. Len: %lu\n", slice.len);
     dr__bundle__drop(bundle);
+    exit(0);
     return 0;
 }
