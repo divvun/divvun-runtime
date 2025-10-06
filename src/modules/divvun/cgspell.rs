12e67b5b7804a1dda8329a6f616cce70b92646a9
Brendan Molloy <brendan@necessary.nu>
2025-10-06 14:34:02 +0200

    cgspell regression fix

--- src/modules/divvun/cgspell.rs
+++ src/modules/divvun/cgspell.rs
@@ -237,7 +237,7 @@ impl CommandRunner for Cgspell {
                     writeln!(&mut out, "\"<{}>\"", c.word_form)
                         .map_err(|e| Error(e.to_string()))?;
 
-                    let is_unknown = c.readings.iter().any(|x| x.tags.contains(&"+?"));
+                    let is_unknown = c.readings.iter().any(|x| x.tags.contains(&"+?") || x.tags.contains(&"?"));
 
                     if !is_unknown {
                         c.readings
