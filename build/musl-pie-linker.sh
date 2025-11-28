4af8e15d8cd46e8f6a01b9b4ac2aba0912daa1c8
Brendan Molloy <brendan@necessary.nu>
2025-11-28 02:43:36 +0100

    hm

--- build/musl-pie-linker.sh
+++ build/musl-pie-linker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Linker wrapper for musl builds that need dynamic GTK.
-# Replaces -static-pie with -pie to allow dynamic library loading
-# while keeping musl libc statically linked.
+# Injects -Bdynamic before -l flags so system libs link dynamically
+# while Rust libs (.rlib) stay static.
 args=()
+found_first_l=false
 for arg in "$@"; do
-  if [ "$arg" = "-static-pie" ]; then
-    args+=("-pie")
-  else
-    args+=("$arg")
+  # Insert -Bdynamic right before the first -l flag
+  if [[ "$arg" == -l* ]] && [ "$found_first_l" = false ]; then
+    args+=("-Wl,-Bdynamic")
+    found_first_l=true
   fi
+  args+=("$arg")
 done
 exec cc "${args[@]}"
