03d5720309b6ff428c675d063d5f46ca1ec993c6
Brendan Molloy <brendan@necessary.nu>
2025-11-28 02:42:26 +0100

    hm

--- build/musl-pie-linker.sh
+++ build/musl-pie-linker.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # Linker wrapper for musl builds that need dynamic GTK.
 # Replaces -static-pie with -pie to allow dynamic library loading
 # while keeping musl libc statically linked.
-exec cc $(echo "$@" | sed 's/-static-pie/-pie/g')
+args=()
+for arg in "$@"; do
+  if [ "$arg" = "-static-pie" ]; then
+    args+=("-pie")
+  else
+    args+=("$arg")
+  fi
+done
+exec cc "${args[@]}"
